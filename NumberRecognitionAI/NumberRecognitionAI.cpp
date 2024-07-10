61581c1c6bc4bfdac1a6af1fef0ba82f0390ecee
Jason <jasongraham8231@gmail.com>
2024-07-10 13:59:21 -0400

    Changed layer sizes to work better

--- NumberRecognitionAI/NumberRecognitionAI.cpp
+++ NumberRecognitionAI/NumberRecognitionAI.cpp
@@ -8,7 +8,7 @@
 int main()
 {
     // Create a neural network object
-    Network network(4, { 784, 6, 6, 10 }, 0.05);
+    Network network(4, { 784, 20, 20, 10 }, 0.05);
 
     // Train the network
     network.learn(32, 10);
